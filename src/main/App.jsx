6e568b239538e63c42f866895e664b1a552e5cef
Eduardo <eduardo.silvafilho@dataprev.gov.br>
2019-11-14 18:01:38 -0300

    Posicionando componentes da tela

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -3,11 +3,13 @@ import React from 'react'
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
+import Main from '../components/template/Main'
 import Footer from '../components/template/Footer'
 
 export default props =>
 <div className="app">
     <Logo />
-    <Nav />    
+    <Nav />   
+    <Main /> 
     <Footer />
 </div>
