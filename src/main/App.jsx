987b4a51eb8399183bd9044e9c7ebf52ec1450b0
Eduardo Filho <filhoeduardo83@gmail.com>
2019-11-14 09:07:28 -0300

    first commit

---
+++ src/main/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import React from 'react'
+
+import Logo from '../components/template/Logo'
+import Nav from '../components/template/Nav'
+import Footer from '../components/template/Footer'
+
+export default props =>
+<div className="app">
+    <Logo />
+    <Nav />    
+    <Footer />
+</div>
