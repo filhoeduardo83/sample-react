bca274ab88b8e1143ba83d63a037ed4fb59f098b
Eduardo Filho <filhoeduardo83@gmail.com>
2019-11-15 15:43:52 -0300

    Incluindo navegação entre os componentes

--- src/main/App.jsx
+++ src/main/App.jsx
@@ -1,18 +1,22 @@
 
 import './App.css'
 import React from 'react'
+import { HashRouter } from 'react-router-dom'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
+
+import Routes from './Routes'
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Main from '../components/template/Main'
 import Footer from '../components/template/Footer'
 
 export default props =>
-<div className="app">
-    <Logo />
-    <Nav />   
-    <Main icon="home" tittle="Titulo"
-        subtitle="Este sistema foi desenvolvido em React"/> 
-    <Footer />
-</div>
+   <HashRouter>
+        <div className="app">
+            <Logo />
+            <Nav />   
+            <Routes />
+            <Footer />
+        </div>
+   </HashRouter> 
